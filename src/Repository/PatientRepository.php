d3bea2359cba0b104c57199958e8b43d4a1d5852
eddyg <eddygomet@gmail.com>
2024-06-28 14:50:20 +0200

    .feat ajout methode repository pour agggrid

--- src/Repository/PatientRepository.php
+++ src/Repository/PatientRepository.php
@@ -17,19 +17,19 @@ class PatientRepository extends ServiceEntityRepository
     }
 
     //    /**
-    //     * @return Patient[] Returns an array of Patient objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    //    //     * @return Patient[] Returns an array of Patient objects
+    //    //     */
+    //    //    public function findByExampleField($value): array
+    //    //    {
+    //    //        return $this->createQueryBuilder('p')
+    //    //            ->andWhere('p.exampleField = :val')
+    //    //            ->setParameter('val', $value)
+    //    //            ->orderBy('p.id', 'ASC')
+    //    //            ->setMaxResults(10)
+    //    //            ->getQuery()
+    //    //            ->getResult()
+    //    //        ;
+    //    //    }
 
     //    public function findOneBySomeField($value): ?Patient
     //    {
@@ -40,4 +40,28 @@ class PatientRepository extends ServiceEntityRepository
     //            ->getOneOrNullResult()
     //        ;
     //    }
+
+    public function findPatientInfoBy(): array
+    {
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQuery(
+            'SELECT p.id, 
+                p.gender, 
+                p.surname, 
+                p.name, 
+                p.birth_date, 
+                p.phone_number,
+                s.bubble_status, 
+                s.type, 
+                s.isActive,
+                s.send_sms
+         FROM App\Entity\Patient p
+         LEFT JOIN p.status s
+         ORDER BY p.id ASC'
+        );
+
+        return $query->getResult();
+    }
+
 }
